cdad37c5f8a34aadb9296dd25f47f8c662fd0d7c
Javier Jimenez <javiyu7@gmail.com>
2014-02-02 09:30:01 +0100

    Fix menu

--- lib/engine/app/controllers/performance_controller.rb
+++ lib/engine/app/controllers/performance_controller.rb
@@ -1,8 +1,15 @@
 class PerformanceController < ApplicationController
+  before_action :load_section
+
   def index
   end
 
   def check
     @stats = PerformanceStats.new(backend).result
+  end
+
+  private
+  def load_section
+    @section = 'performance'
   end
 end
